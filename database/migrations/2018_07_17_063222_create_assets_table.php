a82209ba613c1d9b5af12925e431d73108935077
Amirol Zolkifli <usahaniaga.com@gmail.com>
2018-07-17 14:49:12 +0800

    Update migrations

---
+++ database/migrations/2018_07_17_063222_create_assets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAssetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('barcode');
+            $table->date('tarikh_beli')->nullable();
+            $table->decimal('harga_pasaran', 8, 2);
+            $table->string('jenama');
+            $table->string('jenis');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assets');
+    }
+}
