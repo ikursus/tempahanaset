42b501bba04c4746edb230827cb56a1be85d2f17
Amirol Zolkifli <usahaniaga.com@gmail.com>
2018-07-18 10:58:57 +0800

    Update Delete function

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -102,6 +102,9 @@ class UsersController extends Controller
 
     public function destroy($id)
     {
+        # Panggil data user berdasarkan ID
+        DB::table('users')->where('id', '=', $id)->delete();
+        # Bagi respon redirect ke halaman senarai users
         return redirect()->route('users.index')->with('alert-success', 'Rekod telah berjaya dihapuskan.');
     }
 
